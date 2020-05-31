07b58aef940fd31f28339b2efecc64924d5884b0
Hugo Andres Rosado <u201411897@upc.edu.pe>
2020-05-30 21:37:43 -0500

    More scene added.

---
+++ TempoMonitoring/Scenes/More/Cells/MoreTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MoreTableViewCell.swift
+//  TempoMonitoring
+//
+//  Created by Hugo Andres Rosado on 5/30/20.
+//  Copyright © 2020 Sportafolio SAC. All rights reserved.
+//
+
+import UIKit
+
+class MoreTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
