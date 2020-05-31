c31ac0a46f2b24543e791f7bd6897f37c81bf185
Hugo Andres Rosado <u201411897@upc.edu.pe>
2020-05-30 21:05:32 -0500

    Attention, QRCode and Triage scenes added.

---
+++ TempoMonitoring/Scenes/QRCode/StatusView/QRCodeStatusViewController.swift
@@ -0,0 +1,30 @@
+//
+//  QRCodeStatusViewController.swift
+//  TempoMonitoring
+//
+//  Created by Hugo Andres Rosado on 5/30/20.
+//  Copyright © 2020 Sportafolio SAC. All rights reserved.
+//
+
+import UIKit
+
+class QRCodeStatusViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
