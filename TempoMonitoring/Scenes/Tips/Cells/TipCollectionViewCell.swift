9339f868739b8dde9bac29b18c9c89841ee1ee18
Hugo Andres Rosado <u201411897@upc.edu.pe>
2020-05-31 19:42:54 -0500

    Tips scene added.

---
+++ TempoMonitoring/Scenes/Tips/Cells/TipCollectionViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  TipCollectionViewCell.swift
+//  TempoMonitoring
+//
+//  Created by Hugo Andres Rosado on 5/31/20.
+//  Copyright © 2020 Sportafolio SAC. All rights reserved.
+//
+
+import UIKit
+
+class TipCollectionViewCell: UICollectionViewCell {
+
+    static var reuseIdentifier: String? {
+        return "tipViewCell"
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ TempoMonitoring/Scenes/Tips/Cells/TipsHeaderCollectionReusableView.swift
@@ -0,0 +1,22 @@
+//
+//  TipsHeaderCollectionReusableView.swift
+//  TempoMonitoring
+//
+//  Created by Hugo Andres Rosado on 5/31/20.
+//  Copyright © 2020 Sportafolio SAC. All rights reserved.
+//
+
+import UIKit
+
+class TipsHeaderCollectionReusableView: UICollectionReusableView {
+
+    static var reuseIdentifier: String? {
+        return "tipsHeaderViewCell"
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+}
