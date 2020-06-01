95b4716a3db5e3bfbd56c7d6e9135b9a98d1a24d
Hugo Andres Rosado <u201411897@upc.edu.pe>
2020-05-31 21:51:59 -0500

    Home Banner and New Version popups added.

---
+++ TempoMonitoring/Scenes/Main/NewVersionPopup/NewVersionPopupViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NewVersionPopupViewController.swift
+//  TempoMonitoring
+//
+//  Created by Hugo Andres Rosado on 5/31/20.
+//  Copyright © 2020 Sportafolio SAC. All rights reserved.
+//
+
+import UIKit
+
+class NewVersionPopupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
